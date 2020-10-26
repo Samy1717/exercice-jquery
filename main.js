b43f78819f1dc1167b8631f5b33bf7c05a6a81e1
Samy <c.samy7@hotmail.fr>
2020-10-26 14:38:42 +0100

    add all files

---
+++ main.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+
+    // $("p").hide();
+
+    // $("#test").hide();
+
+    // $(".test").hide();
+
+    // $("*").hide();
+
+    // $("[href]").hide();
+
+    // $("tr:odd").hide();
+
+    // $("p").click(function(){
+    //     $(this).hide();
+    // });
+
+    // $("p").dblclick(function(){
+    //     $(this).hide();
+    // });
+
+    // $("p").mouseenter(function(){
+    //     $(this).hide();
+    // });
+
+    // $("input").keypress(function(){
+    //     $(this).hide();
+    // })
+
+    // $("p").on("click",function(){
+    //     $(this).hide();
+    // });
+
+    // $("p").click(function(){
+    //     $(this).hide();
+    // });
+
+    // $("p").click(function(){
+    //     $(this).hide("slow");
+    // });
+
+    // $("button").click(function(){
+    //     $("p").show();
+    // });
+
+    // $("button").click(function(){
+    //     $("p").toggle();
+    // });
+
+    // $("div").fadeOut();
+
+    // $("div").fadeOut("slow");
+
+    // $("div").fadeTo("slow",0.2);
+
+    // $("button").click(function(){
+    //     $("div").fadeToggle(1000)
+    // })
+
+    // $("div").slideUp();
+
+    // $("div").slideUp("slow");
+
+    // $("div").slideDown();
+
+    // $("button").click(function(){
+    //     $("div").slideToggle(1000)
+    // })
+
+    // $(".test").animate({left:'250px'},2000);
+
+    // $("div").animate({height:'500px'});
+
+    // $("div").animate({
+    //     opacity: '0.4',
+    //     height: '500px',
+    //     width:'500px'
+    // });
+
+    // $("div").animate({fontSize:'100px'},"slow");
+
+    // $("div").stop();
+
+
+    ///////////////////////////////////////////////////
+    // $("div").text();
+
+    // $("div").html();
+
+    // $("input").val();
+
+    // $("a").attr("href");
+
+    ///////////////////////////////////////////////////
+
+    // $("div").text("Hello World");
+
+    // $("input").val("John Doe");
+
+    // $("img").attr("src","image2.jpg");
+    
+    // $("#myLink").text("Demo");
+    // $("#myLink").attr("href", "https://www.demo.com");
+
+    // $("p").append("Apres paragraphe")
+
+    // $("p").prepend("Avant paragraphe")
+
+    // $("p").remove()
+
+    // $("div").empty()
+
+    // $("div").remove(".test,.demo")
+
+    // $("p").addClass("important");
+    
+    // $("p").addClass("important test");
+
+    // $("p").removeClass("important");
+
+    // $("p").click(function(){
+    //     $(this).toggleClass("important");
+    // })
+
+    //Obtenir le parent direct d'un élément <span>
+    // $("span").parent();
+
+    //Obtenir les parents direct d'un élément <span>
+    // $("span").parents();
+
+    //Obtenir tous les enfants directs d'un élément <div>
+    // $("div").children();
+
+    //Obtenir tous les éléments <span> qui sont les descendants de <div>
+    // $("div").find("span");
+
+    //Obtenir tous les éléments frères d'un élément <h2>
+    // $("h2").siblings();
+
+    //Obtenir le premier élément <div> du document
+    // $("div").first();
+
+})
